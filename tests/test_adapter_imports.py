c146df260a3bd8bae21171ec6170e3d65705621b
amazen33 <amazen33@hotmail.com>
2026-09-20 19:04:26 +0300

    Update tests/test_adapter_imports.py - 2026-09-20 19:04

--- tests/test_adapter_imports.py
+++ tests/test_adapter_imports.py
@@ -4,6 +4,6 @@ import importlib
 import pytest
 
 
-@pytest.mark.parametrize("module", ["kafka", "chromadb", "langchain_ollama", "opensearchpy", "elasticsearch"])
+@pytest.mark.parametrize("module", ["kafka", "chromadb", "langchain_ollama", "opensearchpy", "elasticsearch", "azure.storage.blob", "google.cloud.storage"])
 def test_optional_adapter_import(module):
     importlib.import_module(module)
